172cbcfdac3dcbefcfe30678965755d8781344ea
Casey Wilcox <caseywilcox@outlook.com>
2020-08-29 23:04:48 -0400

    update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+import vueCustomElement from "vue-custom-element";
+Vue.use(vueCustomElement);
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+App.router = router;
+Vue.customElement("vue-widget", App);
